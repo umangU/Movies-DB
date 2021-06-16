672585243231d207c6e01495cc39e52f50099f50
umangU <72771903+umangU@users.noreply.github.com>
2021-06-16 23:34:39 +1000

    Code Updated

--- Database-Tables.sql
+++ Database-Tables.sql
@@ -1,4 +1,4 @@
-/* Customer Table */
+/*Customer Table */
 CREATE TABLE Customers (
   customer_id   INTEGER PRIMARY KEY,
   last_name     VARCHAR(50) NOT NULL, 
@@ -9,7 +9,7 @@ CREATE TABLE Customers (
   postcode   VARCHAR(8)
   );
 
-/* Movies Table */                              
+/*Movies Table */                              
 CREATE TABLE Movies (
   movie_id     INTEGER PRIMARY KEY,
   movie_title  VARCHAR(100) NOT NULL,
